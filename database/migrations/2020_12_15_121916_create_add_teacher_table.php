1f8262d6fb679b4751e9bdaaa44cd1876a6bf05c
Cemp412 <chanchalsharma412@gmail.com>
2020-12-15 18:15:44 +0530

    teacher and class table added

---
+++ database/migrations/2020_12_15_121916_create_add_teacher_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddTeacherTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('add_teacher', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('phone_no');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->integer('pincode');
+            $table->integer('institute_id');
+            $table->integer('email');
+            $table->integer('id_proof')->nullable();
+            $table->string('password');
+            $table->boolean('status')->default(0)->comment('0-inactive, 1-active');
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamp('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('add_teacher');
+    }
+}
---
+++ database/migrations/2020_12_15_123534_create_class_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClassTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('class', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('institute_id')->unsigned();
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamp('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('class');
+    }
+}
