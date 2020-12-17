99225a3643172d782075decdf5ffc7f4027f1ba8
Cemp412 <chanchalsharma412@gmail.com>
2020-12-17 17:52:02 +0530

    update method recreated

--- resources/views/SuperAdmin/School/Edit_School.blade.php
+++ resources/views/SuperAdmin/School/Edit_School.blade.php
@@ -6,7 +6,7 @@
                 <div class="card-body">
                     <h5 class="card-title">Add School</h5>
                     <!-- <p>Here’s a quick example to demonstrate Bootstrap’s form styles. </p> -->
-                    <form method="put" action="{{ url('school/'.$school->id) }}" enctype="multipart/form-data">
+                    <form method="put" action="{{ url('school/'.school->id) }}" enctype="multipart/form-data">
                         @csrf
                         <input type="hidden" name="id" value="{{$school->id}}">
                         <div class="form-group">
